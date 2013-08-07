b6ef788cfd370df484a5a8eddcaff4980ebc5db7
Ernest Micklei <ernest.micklei@gmail.com>
2013-08-07 09:28:30 +0200

    remove swagger response type

--- document_resource.go
+++ document_resource.go
@@ -22,14 +22,12 @@ func (d DocumentResource) Register() {
 
 	ws.Route(ws.GET("/").To(d.getAllAliases).
 		Doc("Return all Mongo DB aliases from the configuration").
-		Operation("getAllAliases").
-		Writes(""))
+		Operation("getAllAliases"))
 
 	ws.Route(ws.GET("/{alias}").To(d.getAllDatabaseNames).
 		Doc("Return all database names").
 		Operation("getAllDatabaseNames").
-		Param(alias).
-		Writes(""))
+		Param(alias))
 
 	database := ws.PathParameter("database", "Database name from the MongoDB instance")
 
@@ -37,8 +35,7 @@ func (d DocumentResource) Register() {
 		Doc("Return all collections for the database").
 		Operation("getAllCollectionNames").
 		Param(alias).
-		Param(database).
-		Writes(""))
+		Param(database))
 
 	collection := ws.PathParameter("collection", "Collection name from the database")
 	id := ws.PathParameter("_id", "Storage identifier of the document")
@@ -49,8 +46,7 @@ func (d DocumentResource) Register() {
 		Param(alias).
 		Param(database).
 		Param(collection).
-		Param(id).
-		Writes(""))
+		Param(id))
 
 	ws.Route(ws.PUT("/{alias}/{database}/{collection}/{_id}").To(d.putDocument).
 		Doc("Store a document to a collection from the database using its internal _id").
@@ -59,8 +55,7 @@ func (d DocumentResource) Register() {
 		Param(database).
 		Param(collection).
 		Param(id).
-		Reads("").
-		Writes(""))
+		Reads(""))
 
 	ws.Route(ws.POST("/{alias}/{database}/{collection}").To(d.postDocument).
 		Doc("Store a document to a collection from the database").
@@ -68,8 +63,7 @@ func (d DocumentResource) Register() {
 		Param(alias).
 		Param(database).
 		Param(collection).
-		Reads("").
-		Writes(""))
+		Reads(""))
 
 	ws.Route(ws.GET("/{alias}/{database}/{collection}").To(d.getDocuments).
 		Doc("Return documents (max 10 by default) from a collection from the database.").
@@ -81,8 +75,7 @@ func (d DocumentResource) Register() {
 		Param(ws.QueryParameter("fields", "comma separated list of field names")).
 		Param(ws.QueryParameter("skip", "number of documents to skip in the result set, default=0")).
 		Param(ws.QueryParameter("limit", "maximum number of documents in the result set, default=10")).
-		Param(ws.QueryParameter("sort", "comma separated list of field names")).
-		Writes(""))
+		Param(ws.QueryParameter("sort", "comma separated list of field names")))
 
 	ws.Route(ws.GET("/{alias}/{database}/{collection}/{_id}/{fields}").To(d.getSubDocument).
 		Doc("Get a partial document using the internal _id and fields (comma separated field names)").
@@ -90,9 +83,7 @@ func (d DocumentResource) Register() {
 		Param(alias).
 		Param(database).
 		Param(collection).
-		Param(id).
-		Reads("").
-		Writes(""))
+		Param(id))
 
 	restful.Add(ws)
 }
