8d0ac5e02c668e085827c21741e0921ba466f24a
Ernest Micklei <ernest.micklei@gmail.com>
2013-08-27 16:19:42 +0200

    fixed redirect problem if swagger.path is not set + fixed error message on 500

--- index.go
+++ index.go
@@ -3,9 +3,11 @@ package main
 import "net/http"
 
 func index(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, props["swagger.path"], http.StatusMovedPermanently)
+	if props["swagger.path"] != "" {
+		http.Redirect(w, r, props["swagger.path"], http.StatusMovedPermanently)
+	}
 }
 
 func icon(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, props["swagger.path"] + "images/mora.ico", http.StatusMovedPermanently)
+	http.Redirect(w, r, props["swagger.path"]+"images/mora.ico", http.StatusMovedPermanently)
 }
