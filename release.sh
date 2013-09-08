c06d598e74680c57d66ccde9495bfec27d4e7bae
Ernest Micklei <ernest.micklei@gmail.com>
2013-09-08 21:09:16 +0200

    bin is now called common_request

--- release.sh
+++ release.sh
@@ -4,7 +4,7 @@ go get -v -u github.com/emicklei/go-restful
 go get -v -u labix.org/v2/mgo
 go build *.go
 mkdir -p target/swagger-ui
-mv configuration ./target/mora
+mv common_request ./target/mora
 cp mora.properties ./target
 
 if [ ! -d ./swagger-ui ]; then
