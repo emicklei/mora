601e24bb1d18acb88a363f4907fb596d5df55d9b
Ernest Micklei <ernest.micklei@gmail.com>
2013-09-12 11:16:50 +0200

    work on release script

--- release.sh
+++ release.sh
@@ -1,25 +1,34 @@
-export GOPATH=`pwd`
+#!/bin/sh
+
+# fetch or update all dependent Go packages
+export GOPATH=`pwd`/target
 go get -v -u github.com/emicklei/goproperties
 go get -v -u github.com/emicklei/go-restful
-go get -v -u labix.org/v2/mgo
+go get -v -u labix.org/v2/mgo 
+
+# build Mora
 go build *.go
-mkdir -p target/swagger-ui
+
+# copy binary to target
 mv common_request ./target/mora
 cp mora.properties ./target
 
-if [ ! -d ./swagger-ui ]; then
-  git clone https://github.com/wordnik/swagger-ui.git
+# fetch or update Swagger UI
+if [ ! -d ./target/swagger-ui ]; then
+	git clone https://github.com/wordnik/swagger-ui.git ./target/swagger-ui
 fi
-cp -r ./swagger-ui/dist ./target/swagger-ui/dist
+#cp -r ./swagger-ui/dist ./target/swagger-ui/dist
 
-# apply patches
+# apply customizations to Swagger UI
 cp patches/index.html ./target/swagger-ui/dist
 cp patches/logo_small.png ./target/swagger-ui/dist/images
 cp patches/mora.ico ./target/swagger-ui/dist/images
 sed "s/89bf04/89bfAA/" ./target/swagger-ui/dist/css/screen.css > ./target/swagger-ui/dist/css/screen-mora.css
 
+ARCHIVE="mora-"`date +"%Y%m%d"`".zip"
 cd target
-	rm -f mora.zip
-	zip -r mora.zip .
+	rm -f *.zip
+	zip $ARCHIVE mora
+	zip $ARCHIVE mora.properties
+	zip -r $ARCHIVE ./swagger-ui/dist
 	cd ..
-ls -l target
