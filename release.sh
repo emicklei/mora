88f1939f4c6599cc2bb5ac8ca601d6ae6554bfc7
Ernest Micklei <emicklei@Ernests-iMac.local>
2016-01-17 14:33:47 +0100

    fixed and simplified release.sh, see issue #36 and #38

--- release.sh
+++ release.sh
@@ -1,30 +1,27 @@
 #!/bin/sh
 
+rm -rf target
+mkdir target
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# fetch or update all dependent Go packages
-export GOPATH=`pwd`/target
-go get -v -u github.com/emicklei/goproperties
+go get -v -u github.com/magiconair/properties
 go get -v -u github.com/emicklei/go-restful
 go get -v -u labix.org/v2/mgo 
 
 # build Mora
-go build *.go
-
-# copy binary to target
-mv common_request ./target/mora
-cp mora.properties ./target
+go build -o target/mora main.go
+cp mora.properties target
 
 # fetch or update Swagger UI
 if [ ! -d ./target/swagger-ui ]; then
 	git clone https://github.com/wordnik/swagger-ui.git ./target/swagger-ui
 fi
-#cp -r ./swagger-ui/dist ./target/swagger-ui/dist
 
 # apply customizations to Swagger UI
-cp $DIR/patches/index.html ./target/swagger-ui/dist
-cp $DIR/patches/logo_small.png ./target/swagger-ui/dist/images
-cp $DIR/patches/mora.ico ./target/swagger-ui/dist/images
+cp $DIR/scripts/patches/index.html ./target/swagger-ui/dist
+cp $DIR/scripts/patches/logo_small.png ./target/swagger-ui/dist/images
+cp $DIR/scripts/patches/mora.ico ./target/swagger-ui/dist/images
 sed "s/89bf04/89bfAA/" ./target/swagger-ui/dist/css/screen.css > ./target/swagger-ui/dist/css/screen-mora.css
 
 ARCHIVE="mora-"`date +"%Y%m%d"`".zip"
